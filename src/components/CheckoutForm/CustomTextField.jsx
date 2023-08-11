fcc2e9eb2b8df68219a239871b76b094f18f1629
deepak-wednesday <deepak@wednesday.is>
2023-08-11 13:54:51 +0530

    feat: book store

---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { useFormContext, Controller } from "react-hook-form";
+import { TextField, Grid } from "@material-ui/core";
+
+function FormInput({ name, label, required }) {
+  const { control } = useFormContext();
+  const isError = false;
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+        as={TextField}
+        name={name}
+        control={control}
+        label={label}
+        fullWidth
+        required={required}
+        error={isError}
+      />
+    </Grid>
+  );
+}
+
+export default FormInput;
---
+++ src/lib/commerce.js
@@ -0,0 +1,6 @@
+import Commerce from "@chec/commerce.js";
+
+export const commerce = new Commerce(
+  "pk_test_286636a55c5f265ec761c9586095217fb03c5602a7ae7",
+  true
+);
